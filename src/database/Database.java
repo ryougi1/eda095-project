9ed00f91982e04ce64f7cb4a28fd140168d09461
James Gu <dic14jgu@falk-6.student.lth.se>
2016-04-02 18:05:25 +0200

    continued work on Database class

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,8 +1,7 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException; 
+import java.sql.*;
+import java.util.ArrayList;
 
 import database.Database;
 
@@ -14,9 +13,11 @@ public class Database {
 
 	private Connection conn = null;
 	private String sql;
+	private static ArrayList<String> cookies;
 
 	public Database() throws ClassNotFoundException, SQLException {
 		Class.forName(JDBC_DRIVER); //Register JDBC drivers
+		cookies = new ArrayList<String>();
 	}
 	
 	/*
@@ -37,9 +38,36 @@ public class Database {
 		System.out.println("Connection terminated.");
 	}
 	
+	public void getCookies() {
+		String sql = "select cookieName from Cookies";
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				cookies.add(rs.getString("cookieName"));
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+			} catch (SQLException e2) {
+				// ... can do nothing if things go wrong here
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Database db = new Database();
 		db.establishConnection();
-		db.terminateConnection();
+		db.getCookies();
+		for (int i = 0; i < cookies.size(); i++) {
+			System.out.println(cookies.get(i));
+		}
+//		db.terminateConnection();
 	}
+	
+	
 }
