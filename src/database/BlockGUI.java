7ae4a7ee0533e72acde54a0232af24acf5cdd0ec
Turbobollen <dat13jbo@student.lu.se>
2016-04-10 03:35:41 +0200

    90% finished with BlockGUI

--- src/database/BlockGUI.java
+++ src/database/BlockGUI.java
@@ -1,13 +1,17 @@
 package database;
 
-
+import java.sql.SQLException;
 import java.util.Locale;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -19,77 +23,167 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class BlockGUI extends Application {
+	private Database db;
+	private DatePicker startDate;
+	private DatePicker endDate;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(final Stage stage) throws Exception {
 		Locale.setDefault(Locale.US);
 		stage.setTitle("Block pallets");
 
 		stage.getIcons().add(new Image("file:icon.jpg"));
-		
+
 		stage.setHeight(350);
 		stage.setWidth(1400);
 		stage.setResizable(false);
-		
+
 		BorderPane layout = new BorderPane();
-		
+
 		GridPane grid = new GridPane();
 		grid.setPadding(new Insets(20, 0, 20, 20));
 		grid.setHgap(7);
 		grid.setVgap(7);
 
-		Label title = new Label("Select which cookie and during what time intervall to block.");
+		Label title = new Label(
+				"Select which cookie and during what time intervall to block.");
 
-		DatePicker startDate = new DatePicker();
-		DatePicker endDate = new DatePicker();
+		startDate = new DatePicker();
+		endDate = new DatePicker();
 
-		RestrictiveTextField startHour = new RestrictiveTextField();
+		final RestrictiveTextField startHour = new RestrictiveTextField();
 		startHour.setMaxLength(2);
 		startHour.setRestrict("[0-9]");
 		startHour.setPromptText("Hour");
-		RestrictiveTextField startMin = new RestrictiveTextField();
+		final RestrictiveTextField startMin = new RestrictiveTextField();
 		startMin.setMaxLength(2);
 		startMin.setRestrict("[0-9]");
 		startMin.setPromptText("Minute");
-		
-		RestrictiveTextField endHour = new RestrictiveTextField();
+
+		final RestrictiveTextField endHour = new RestrictiveTextField();
 		endHour.setMaxLength(2);
 		endHour.setRestrict("[0-9]");
 		endHour.setPromptText("Hour");
-		RestrictiveTextField endMin = new RestrictiveTextField();
+		final RestrictiveTextField endMin = new RestrictiveTextField();
 		endMin.setMaxLength(2);
 		endMin.setRestrict("[0-9]");
 		endMin.setPromptText("Minute");
-		
-		
-		Button block = new Button("Block");
-		Button cancel = new Button("Cancel");
-		
-		
-		grid.add(new Label("Start "),0,1);
+
+		grid.add(new Label("Start "), 0, 1);
 		grid.add(startDate, 1, 1);
 		grid.add(startHour, 2, 1);
 		grid.add(new Label(":"), 3, 1);
 		grid.add(startMin, 4, 1);
-		grid.add(new Label("End "),0,2);
+		grid.add(new Label("End "), 0, 2);
 		grid.add(endDate, 1, 2);
 		grid.add(endHour, 2, 2);
 		grid.add(new Label(":"), 3, 2);
 		grid.add(endMin, 4, 2);
-		
+
+		Button block = new Button("Block");
+		block.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Alert warning = new Alert(AlertType.WARNING);
+				warning.setTitle("Warning");
+				warning.setContentText("Invalid Date or Time input!");
+				boolean invalid = false;
+				
+				String startDateTime = new String();
+				int startH = -1;
+				int startM = -1;
+				if (!startHour.getText().trim().isEmpty()) {
+					startH = Integer.parseInt(startHour.getText());
+				} else {
+					invalid = true;
+				}
+				if (!startMin.getText().trim().isEmpty()) {
+					startM = Integer.parseInt(startMin.getText());
+				} else {
+					invalid = true;
+				}
+				if (startDate.getValue() != null && startH >= 0 && startH <= 23
+						&& startM >= 0 && startM <= 59) {
+					String startHour;
+					String startMin;
+					if (startH < 10) {
+						startHour = "0" + startH;
+					} else {
+						startHour = "" + startH;
+					}
+					if (startM < 10) {
+						startMin = "0" + startM;
+					} else {
+						startMin = "" + startM;
+					}
+					startDateTime = startDate.getValue().toString()
+							+ " " + startHour + ":" + startMin;
+					System.out.println(startDateTime);
+				} else {
+					invalid = true;
+				}
+				String endDateTime = new String();
+				int endH = -1;
+				int endM = -1;
+				if (!endHour.getText().trim().isEmpty()) {
+					endH = Integer.parseInt(endHour.getText());
+				} else {
+					invalid = true;
+				}
+				if (!endMin.getText().trim().isEmpty()) {
+					endM = Integer.parseInt(endMin.getText());
+				} else {
+					invalid = true;
+				}
+				if (endDate.getValue() != null && endH >= 0 && endH <= 23
+						&& endM >= 0 && endM <= 59) {
+					String endHour;
+					String endMin;
+					if (endH < 10) {
+						endHour = "0" + endH;
+					} else {
+						endHour = "" + endH;
+					}
+					if (endM < 10) {
+						endMin = "0" + endM;
+					} else {
+						endMin = "" + endM;
+					}
+					endDateTime = endDate.getValue().toString()
+							+ " " + endHour + ":" + endMin;
+					System.out.println(endDateTime);
+				} else {
+					invalid = true;
+				}
+				if(!invalid){
+					db.blockPalletByTime(startDateTime, endDateTime);	
+					System.out.println("Perfect!");
+				} else {
+					warning.showAndWait();
+				}
+			}
+		});
+		Button cancel = new Button("Cancel");
+		cancel.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.close();
+			}
+		});
+
 		HBox top = new HBox();
 		top.setAlignment(Pos.CENTER);
 		top.getChildren().add(title);
-		
+
 		HBox hb = new HBox();
 		hb.setAlignment(Pos.CENTER);
 		hb.setSpacing(15);
 		hb.getChildren().addAll(block, cancel);
-		
+
 		layout.setTop(top);
 		layout.setCenter(grid);
 		layout.setBottom(hb);
@@ -97,6 +191,8 @@ public class BlockGUI extends Application {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void setDatabase(Database db) {
+		this.db = db;
+	}
 }
-
-
